d4a4b3fe15fac4bd24be5c3950986e506398d9f7
sh4rkd <20006132@es.uveg.edu.mx>
2022-11-27 20:50:31 -0700

    variables en flask

--- holamundo.py
+++ holamundo.py
@@ -12,3 +12,7 @@ def lala():
 @app.route("/lala/<nombre>")
 def lala_nombre(nombre):
     return "Lala " + nombre
+
+@app.route("/post/<int:post_id>")
+def show_post(post_id):
+    return f"El id del post es: {post_id}"
