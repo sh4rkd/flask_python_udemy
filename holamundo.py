bb0490a909f5dda4e6ab2d04856b85e164d68523
sh4rkd <20006132@es.uveg.edu.mx>
2022-11-28 10:06:05 -0700

    formularios

--- holamundo.py
+++ holamundo.py
@@ -1,18 +1,25 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
 @app.route("/")
 def index():
     return "Hola Mundo!"
 
-@app.route("/lala")
-def lala():
+@app.route("/lele", methods=['POST'])
+def lele():
+    print(request.form)
+    print(request.form['llave1'])
+    print(request.form['llave2'])
     return "Lala"
 
 @app.route("/lala/<nombre>")
 def lala_nombre(nombre):
     return "Lala " + nombre
 
-@app.route("/post/<int:post_id>")
+#GET, POST, PUT, PATCH, DELETE
+@app.route("/post/<int:post_id>", methods=['GET', 'POST'])
 def show_post(post_id):
-    return f"El id del post es: {post_id}"
+    if request.method == 'POST':
+        return f"POST {post_id}"
+    else:
+        return f"GET {post_id}" 
